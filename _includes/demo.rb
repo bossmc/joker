e7736ebf7398b6d7cb83aaf1e9376c96a6e6bd2b
karottenreibe <karottenreibe@gmail.com>
2009-09-09 18:28:46 +0200

    added stuff to demo

--- _includes/demo.rb
+++ _includes/demo.rb
@@ -1,5 +1,22 @@
 require 'rubygems'
 require 'joker'
 
-Wildcard['']
+
+wild = Wildcard['Fairy?ake*']
+
+wild =~ 'Fairycake'                     #=> true
+wild =~ 'Fairyfakes'                    #=> true
+wild =~ 'Fairylake is a cool place'     #=> true
+
+wild =~ 'Dairycake'                     #=> false
+wild =~ 'Fairysteakes'                  #=> false
+wild =~ 'fairycake'                     #=> false
+
+wildi = Wildcard['Fairy?ake*\?', true]
+
+wildi =~ 'FairyCake?'                   #=> true
+wildi =~ 'fairyfakes?'                  #=> true
+wildi =~ 'FairyLake IS A COOL Place?'   #=> true
+
+Wildcard.quote('*?\\')                  #=> '\\*\\?\\\\'
 
