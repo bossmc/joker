56a7564c0adaee2c15879935fba496716f9078b3
karottenreibe <karottenreibe@gmail.com>
2009-10-15 21:29:48 +0200

    updated C documentation so rdoc can pick it up

--- ext/joker_native/Joker.c
+++ ext/joker_native/Joker.c
@@ -14,7 +14,28 @@ void Init_joker_native(void)  // {{{1
 }
 
 
-VALUE instance_operator_match(self, object)  // {{{1
+/*
+ * call-seq:
+ *   wildcard =~  'string' -> true or false
+ *   'string' =~  wildcard -> true or false
+ *   wildcard === 'string' -> true or false
+ *
+ * Matches the Wildcard against the given string.
+ *
+ * NOTE: Since a wildcard has to match the whole string,
+ * this method only returns true or false, not the position
+ * of the match.
+ *
+ *   Wildcard['*fairy*'] =~  'I love fairycake'    #=> true
+ *   'I love fairycake'  =~  Wildcard['*dairy*']   #=> false
+ *
+ *   case 'I love fairycake'
+ *   when Wildcard['*fairy*'] then puts 'fairy!'
+ *   else puts 'no fairy...'
+ *   end
+ *
+ */
+VALUE instance_operator_match(self, object)
     VALUE  self;
     VALUE  object;
 {
@@ -38,7 +59,21 @@ VALUE instance_operator_match(self, object)  // {{{1
 }
 
 
-VALUE class_method_new(argc, argv, klass)  // {{{1
+/*
+ * call-seq:
+ *   Wildcard.new(wildcard_string, casefold = false) -> Wildcard
+ *
+ * Creates a new Wildcard from the given string.
+ * If casefold is true, the Wildcard will ignore case.
+ *
+ * Raisess a SyntaxError if the given string could not
+ * be interpreted as a Wildcard.
+ *
+ * Issues warnings to the console if the given Wildcard
+ * was malformed.
+ *
+ */
+VALUE class_method_new(argc, argv, klass)
     int      argc;
     VALUE *  argv;
     VALUE    klass;
--- ext/joker_native/Joker.h
+++ ext/joker_native/Joker.h
@@ -9,41 +9,14 @@ void Init_joker(void);
 
 
 /*
- * call-seq:
- *   Wildcard.new(wildcard_string, casefold = false) -> Wildcard
- *
- * Creates a new Wildcard from the given string.
- * If casefold is true, the Wildcard will ignore case.
- *
- * Raisess a SyntaxError if the given string could not
- * be interpreted as a Wildcard.
- *
- * Issues warnings to the console if the given Wildcard
- * was malformed.
+ * This function will become the Wildcard::new method.
  *
  */
 VALUE class_method_new(int argc, VALUE * argv, VALUE klass);
 
 
 /*
- * call-seq:
- *   wildcard =~  'string' -> true or false
- *   'string' =~  wildcard -> true or false
- *   wildcard === 'string' -> true or false
- *
- * Matches the Wildcard against the given string.
- *
- * NOTE: Since a wildcard has to match the whole string,
- * this method only returns true or false, not the position
- * of the match.
- *
- *   Wildcard['*fairy*'] =~  'I love fairycake'    #=> true
- *   'I love fairycake'  =~  Wildcard['*dairy*']   #=> false
- *
- *   case 'I love fairycake'
- *   when Wildcard['*fairy*'] then puts 'fairy!'
- *   else puts 'no fairy...'
- *   end
+ * This function will become the =~ and === methods.
  *
  */
 VALUE instance_operator_match(VALUE self, VALUE string);
